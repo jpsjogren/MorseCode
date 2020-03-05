8050cd934590bf9f1757a145c49d58303e3f8e0a
jpsjogren <52937165+jpsjogren@users.noreply.github.com>
2020-03-05 16:55:07 +0100

    Tjoho

--- MorseCode/src/Tests/TestInput.java
+++ MorseCode/src/Tests/TestInput.java
@@ -1,4 +1,5 @@
 package Tests;
+import Program.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,9 +7,31 @@ import org.junit.jupiter.api.Test;
 
 class TestInput {
 
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testTextToCodeVerificationUnder940() {
+		String oneWord = "pneumonoultramicroscopicsilicovolcanoconiosis"; //46 chars long times 20 words = 920 chars.
+		String textString = "";
+		for (int i = 0; i < 20; i++) {
+			textString = oneWord + " " + textString;
+		}
+		
+		TakeInput takeInput = new TakeInput();
+		textString = takeInput.toCodeVerification(textString);
+		
+		assert textString.length() <= 940;
+		
+	}
+	@Test
+	void testTextToCodeVerificationNotEmpty() {
+		String textString = "";
+		
+		TakeInput takeInput = new TakeInput();
+		textString = takeInput.toCodeVerification(textString);
+		
+		assert textString.length() > 0;
+		
 	}
 
 }
