a9ffe1b68f4a84c106de9d408315e05ac2af2ef5
alexanderbram <alexnader.bram@utb.ecutbildning.se>
2020-03-05 15:41:39 +0100

    fuck2

--- MorseCode/src/Program/Program.java
+++ MorseCode/src/Program/Program.java
@@ -5,7 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < 100; i++) {
-			System.out.println(i + "hello fucking world.");
+			System.out.println(i + "hello fucking world");
 		}
 	}
 
