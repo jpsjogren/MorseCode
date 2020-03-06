8902cfde2bdafdc1dc8300eab6e79e7f356fecbf
alexanderbram <alexnader.bram@utb.ecutbildning.se>
2020-03-06 12:07:34 +0100

    h

--- MorseCode/src/Program/Translator.java
+++ MorseCode/src/Program/Translator.java
@@ -1,5 +1,7 @@
 package Program;
 
+import java.util.Scanner;
+
 public class Translator {
 	
 	char[] letters = {' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
@@ -27,6 +29,35 @@ public class Translator {
 	
 	public String morseToEng() {
 		return null;
+		
+	}
+	
+	//
+	boolean update = true;
+	Scanner input = new Scanner(System.in);
+	String toEng = "";
+	
+
+	System.out.println("Write one sequence in morse at a time, press enter after each letter/digit.");
+	System.out.println("Enter 4 blank spaces and press enter after each word, to finish enter '0' and press enter.\n");
+	
+	
+	
+	while (update) {
+		String tempInput = input.nextLine();
+		
+		if (tempInput.equals("quit")) {
+			update = false;
+			System.out.println(toEng);
+		}
+		else {
+			for (int i = 0; i < morseLetters.length; i++) {
+				if (tempInput.equals(morseLetters[i])) {
+					toEng += letters[i];
+					System.out.println(toEng);
+				}
+			}
+		}
 		
 	}
 
