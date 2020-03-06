4deed1022d61d77f4c0f46538dcc41226e332ed3
Jocke <joachim.sjogren@gmil.com>
2020-03-06 11:14:37 +0100

    .

--- MorseCode/src/Program/Translator.java
+++ MorseCode/src/Program/Translator.java
@@ -14,7 +14,7 @@ public class Translator {
 			 ".....", "-....", "--...", "---..", "----.",
 			 "-----",".-.-.-","--..--","..--.."};
 	
-	/*
+	
 	public String charToMorse(char testInput) {
 		
 		for (int i = 0; i < letters.length; i++) {
@@ -24,14 +24,11 @@ public class Translator {
 		}
 		return null;
 	}
-	*/
+	
 	public String morseToEng() {
 		return null;
 		
 	}
 
-	public String charToMorse(char c) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
 }
