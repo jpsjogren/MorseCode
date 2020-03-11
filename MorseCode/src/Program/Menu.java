2f806e37b979433f885389cbaf81f22c3c259909
alexanderbram <alexnader.bram@utb.ecutbildning.se>
2020-03-11 18:09:01 +0100

    Meny nästan klar, behövs lite felhanterig

--- MorseCode/src/Program/Menu.java
+++ MorseCode/src/Program/Menu.java
@@ -1,10 +1,12 @@
 package Program;
 
+import java.util.Scanner;
+
 public class Menu {
 	
 	
 	
-	private void showStart() {
+	public static void showStart() throws InterruptedException {
 		System.out.println("-- Welcome to the Dih-Dah-Translator --");
 		System.out.println("---------------------------------------");
 		System.out.println("Would you like to translate: ----------");
@@ -13,10 +15,61 @@ public class Menu {
 		System.out.println("3: Help -------------------------------");
 		System.out.println("4: Exit -------------------------------");
 		System.out.println("---------------------------------------");
+		menuControl();
 		}
-	
-	
-	private void showHelp() {
+	public static void menuControl() throws InterruptedException {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Press the digit corresponding to your choice, then press enter");
+		int input = scanner.nextInt();
+		switch(input) {
+		case 1:
+			initializeMorseToText();
+			Thread.sleep(3000);
+			showStart();
+			break;
+		
+		case 2: 
+			initializeTextToMorse();
+			Thread.sleep(3000);
+			showStart();
+			break;
+		case 3:
+			showHelp();
+			Thread.sleep(3000);
+			showStart();
+			break;
+		case 4:
+			Shutdown();
+			break;
+		default:
+			System.out.println("Invalid, try again.");
+			showStart();
+		}
+	}
+	public static void Shutdown() throws InterruptedException {
+		System.out.print("Shutting down...");
+		Thread.sleep(2000);
+		System.exit(0);
+	}
+	public static void initializeMorseToText() {
+		Scanner scanner = new Scanner(System.in);
+		Translate userTranslate = new Translate();
+		
+		System.out.println("Enter the morse code you want to translate: ");
+		String userInputMorse = scanner.nextLine();
+		String outputText = userTranslate.codeToText(userInputMorse);
+		System.out.println("Result: \n" + outputText);
+	}
+	public static void initializeTextToMorse() {
+		Scanner scanner = new Scanner(System.in);
+		Translate userTranslate = new Translate();
+		
+		System.out.println("Enter the text you want to translate: ");
+		String userInputText = scanner.nextLine();
+		String outputMorse = userTranslate.textToCode(userInputText);
+		System.out.println("Result: \n" + outputMorse);
+	}
+	public static void showHelp() {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to our translator!");
 		System.out.println("");
--- MorseCode/src/Program/Program.java
+++ MorseCode/src/Program/Program.java
@@ -2,11 +2,11 @@ package Program;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
 		Menu.showStart();
-		
+		Menu.menuControl();
 		
 	}
 
