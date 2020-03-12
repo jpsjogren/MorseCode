909006a8171b83e0c5e541103af88d1bdaf26bef
Jocke <joachim.sjogren@gmil.com>
2020-03-12 11:08:20 +0100

    .

--- MorseCode/src/Program/Program.java
+++ MorseCode/src/Program/Program.java
@@ -3,9 +3,8 @@ package Program;
 public class Program {
 
 	public static void main(String[] args) throws InterruptedException {
-		// TODO Auto-generated method stub
 		
-	
+		
 		Menu.menuControl();
 		
 	}
